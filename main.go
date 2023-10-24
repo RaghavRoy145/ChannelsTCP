779d53360d22190ec9cbf75227f15d992739e8dd
Raghav Roy <raghavroy145@gmail.com>
2023-10-24 16:52:54 +0530

    send and receive channel pairs are handled by one structure

--- main.go
+++ main.go
@@ -9,17 +9,25 @@ import (
 )
 
 func main() {
-	receiver, err := tcpchan.NewReceiver[int](":3000")
+	channelLocal, err := tcpchan.New[string](":3000", ":4000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// The reason we are using sleep here (and below) is because in a real
+	// scenario, we would would have the send and receive pairs on different
+	// servers, on the same machine we need to wait to ensure everyone is
+	// ready and we are not blocking on a server
+
+	go func() {
+		time.Sleep(time.Second * 5)
+		channelLocal.SendCh <- "royboi"
+	}()
+	channelRemote, err := tcpchan.New[string](":4000", ":3000")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	sender, err := tcpchan.NewSender[int](":3000")
-	if err != nil {
-		log.Fatal(err)
-	}
-	time.Sleep(time.Second * 2)
-	sender.Chan <- 100
-	msg := <-receiver.Chan
+	time.Sleep(time.Second * 3)
+	msg := <-channelRemote.RecvCh
 	fmt.Println("received from channel over the wire: ", msg)
 }
--- tcpchan/tcpchan.go
+++ tcpchan/tcpchan.go
@@ -7,93 +7,77 @@ import (
 	"time"
 )
 
-var defaultDialInterval = 3 * time.Second
-
 // generics, woo
-type Sender[T any] struct {
-	Chan chan T
-
-	remoteAddr   string
-	outboundConn net.Conn
-	dialInterval time.Duration
-}
-
-type Receiver[T any] struct {
-	Chan       chan T
+type TCPChan[T any] struct {
 	listenAddr string
-	listener   net.Listener
+	remoteAddr string
+
+	SendCh       chan T
+	RecvCh       chan T
+	outboundConn net.Conn
+	ln           net.Listener
 }
 
-func NewSender[T any](remoteAddr string) (*Sender[T], error) {
-	sender := &Sender[T]{
-		Chan:         make(chan T),
-		remoteAddr:   remoteAddr,
-		dialInterval: defaultDialInterval,
-	}
-	go sender.dialRemote()
-	go sender.loop()
-
-	return sender, nil
-}
-
-func NewReceiver[T any](listenAddr string) (*Receiver[T], error) {
-	recv := &Receiver[T]{
-		Chan:       make(chan T),
+func New[T any](listenAddr, remoteAddr string) (*TCPChan[T], error) {
+	tcpchan := &TCPChan[T]{
 		listenAddr: listenAddr,
+		remoteAddr: remoteAddr,
+		SendCh:     make(chan T, 10),
+		RecvCh:     make(chan T, 10),
 	}
-
 	ln, err := net.Listen("tcp", listenAddr)
 	if err != nil {
 		return nil, err
 	}
-	recv.listener = ln
+	tcpchan.ln = ln
 
-	go recv.acceptLoop()
-	return recv, nil
+	go tcpchan.loop()
+	go tcpchan.acceptLoop()
+	go tcpchan.dialRemoteAndRead()
+	return tcpchan, nil
 }
 
-func (r *Receiver[T]) acceptLoop() {
+func (t *TCPChan[T]) loop() {
+	for {
+		msg := <-t.SendCh
+		if err := gob.NewEncoder(t.outboundConn).Encode(&msg); err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func (t *TCPChan[T]) acceptLoop() {
 	defer func() {
-		r.listener.Close()
+		t.ln.Close()
 	}()
 	for {
-		conn, err := r.listener.Accept()
+		conn, err := t.ln.Accept()
 		if err != nil {
 			log.Println("Accept error", err)
 			return
 		}
 		log.Printf("sender connected %s", conn.RemoteAddr())
-		go r.handleConn(conn)
+		go t.handleConn(conn)
 	}
 }
 
-func (r *Receiver[T]) handleConn(conn net.Conn) {
+func (t *TCPChan[T]) handleConn(conn net.Conn) {
 	for {
 		var msg T
 		if err := gob.NewDecoder(conn).Decode(&msg); err != nil {
 			log.Println(err)
 			continue
 		}
-		r.Chan <- msg
+		t.RecvCh <- msg
 	}
 }
 
-func (s *Sender[T]) dialRemote() {
-	conn, err := net.Dial("tcp", s.remoteAddr)
+func (t *TCPChan[T]) dialRemoteAndRead() {
+	conn, err := net.Dial("tcp", t.remoteAddr)
 	if err != nil {
-		log.Printf("dial error (%s) retrying in (%d): ", err, s.dialInterval)
-		time.Sleep(s.dialInterval)
-		s.dialRemote()
+		log.Printf("dial error (%s) retrying: ", err)
+		time.Sleep(time.Second * 3)
+		t.dialRemoteAndRead()
 	}
-	s.outboundConn = conn
-}
-
-func (s *Sender[T]) loop() {
-	for {
-		msg := <-s.Chan
-		log.Println("sending msg over the wire: ", msg)
-		if err := gob.NewEncoder(s.outboundConn).Encode(msg); err != nil {
-			log.Println(err)
-		}
-	}
+	t.outboundConn = conn
 }
